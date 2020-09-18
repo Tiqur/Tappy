e7aa1421cb1babb4fbe4c7f57b38f646dbfde428
Tiqur <Tiqur7@gmail.com>
2020-09-18 01:19:49 -0700

    Fixed graph and added UR ( broken atm )

--- app/src/main/java/me/tiqur/tappy/MainActivity.java
+++ app/src/main/java/me/tiqur/tappy/MainActivity.java
@@ -1,69 +1,56 @@
 package me.tiqur.tappy;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-import java.util.ArrayList;
+import java.text.DecimalFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashSet;
 
-class Click {
-    long time;
-    boolean key; // true = left, false = right
-
-    public Click(long now, boolean b) {
-        time = now;
-        key = b;
-    }
-}
 
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     long startTime = -1L;
-    int bpmAverageRange = 2000; // ( Milliseconds ) this will be dynamic later on  ( example: if 2000ms, then get every click from last 2sec and average )
-    HashSet<Click> clicks = new HashSet<>();
-    int b1clicks;
-    int b2clicks;
-    int bpmTarget = 180;
-    private Button button1;
-    private LineGraphSeries<DataPoint> averageBpmGraphSeries;
-    private Button button2;
+    int bpmAverageRangeInterval = 2000; // ( Milliseconds ) this will be dynamic later on  ( example: if 2000ms, then get every click from last 2sec and average )
+    HashSet<Long> clicks = new HashSet<>();
+    boolean showEntireBpmAverageGraph = false;
+    private LineGraphSeries<DataPoint> averageBpm;
+    private LineGraphSeries<DataPoint> unstableRate;
+    private GraphView graphView;
     private Button resetButton;
+    private Button button1;
+    private Button button2;
     private SeekBar bpmViz1;
+    private TextView clicksText;
+    private TextView unstableRateText;
     private TextView target;
     private TextView bpmText;
-    private TextView clicksText;
+    private Double UR;
+    int bpmTarget = 180;
+    int b1clicks;
+    int b2clicks;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        GraphView averageBpmGraphView = (GraphView) findViewById(R.id.graph);
-        averageBpmGraphSeries = new LineGraphSeries<DataPoint>();
-
-        double x, y;
-        x = -5.0;
-        for (int i = 0; i < 500; i++) {
-            x = x + 0.1;
-            y = Math.sin(x);
-            averageBpmGraphSeries.appendData(new DataPoint(x, y), true, 500);
-        }
-        averageBpmGraphView.addSeries(averageBpmGraphSeries);
-
 
         button1 = (Button) findViewById(R.id.button1);
         button2 = (Button) findViewById(R.id.button2);
@@ -71,12 +58,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         resetButton = (Button) findViewById(R.id.resetBtn);
         bpmText = (TextView) findViewById(R.id.bpm);
         clicksText = (TextView) findViewById(R.id.clicks);
+        unstableRateText = (TextView) findViewById(R.id.unstableRateText);
         target = (TextView) findViewById(R.id.targetText);
+        graphView = (GraphView) findViewById(R.id.bpmGraph);
+        averageBpm = new LineGraphSeries<DataPoint>();
+        unstableRate = new LineGraphSeries<DataPoint>();
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
 
         bpmViz1.setMax(bpmTarget*2);
         target.setText("Target: " + bpmTarget);
+
+        graphInit();
 
         // I should probably fix this at some point
         resetButton.setOnClickListener(new View.OnClickListener() {
@@ -92,6 +85,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 button2.setText("");
             }
         });
+
+
+
+    }
+
+    private void graphInit() {
+        averageBpm.setTitle("BPM");
+        unstableRate.setTitle("Unstable Rate");
+        graphView.addSeries(unstableRate);
+        graphView.addSeries(averageBpm);
+        unstableRate.setColor(Color.RED);
+        Viewport viewport = graphView.getViewport();
+        viewport.setYAxisBoundsManual(true);
+        viewport.setMinY(0);
+        viewport.setMaxY(bpmTarget*2);
+        viewport.setMaxX(20);
+        viewport.setScalable(true);
+        graphView.getGridLabelRenderer().setHumanRounding(false);
+        graphView.getLegendRenderer().setVisible(true);
+        graphView.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+        graphView.onDataChanged(false, false);
     }
 
     // unstable rate
@@ -116,32 +130,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
         // array of clicks that are IN THE SPECIFIED RANGE at this current time
-        HashSet<Click> inRange = new HashSet<>();
+        HashSet<Long> inRange = new HashSet<>();
         // add the clicks that are in range to the inRange set
-        for (Click click : clicks) if (click.time > now - bpmAverageRange) inRange.add(click);
+        for (Long clickTime : clicks) if (clickTime > now - bpmAverageRangeInterval) inRange.add(clickTime);
 
         // adds current click to both inRange, and normal set
-        Click currentClick = new Click(now, v.getId() == button1.getId());
-        clicks.add(currentClick);
-        inRange.add(currentClick);
+        clicks.add(now);
+        inRange.add(now);
 
         // Calculates BPM
-        double bpm = Math.round((((float) inRange.size() / bpmAverageRange * 60000.0) / 4.0) * 100.0) / 100.0;;
+        double bpm = Math.round((((float) inRange.size() / bpmAverageRangeInterval * 60000.0) / 4.0) * 100.0) / 100.0;;
 
         // moves according to bpm
         bpmViz1.setProgress((int)bpm, true);
 
+        // get UR
+        UR = calcStandardDeviation(inRange);
 
 
+        // append data to graph
+        averageBpm.appendData(new DataPoint(clicks.size(), bpm), !showEntireBpmAverageGraph, Integer.MAX_VALUE);
+        unstableRate.appendData(new DataPoint(clicks.size(), UR), !showEntireBpmAverageGraph, Integer.MAX_VALUE);
+
+
+        // increase max graph size
+        if (showEntireBpmAverageGraph) graphView.getViewport().setMaxX(clicks.size()+10);
+
+        // update each of the label's text on each click
+        updateText(v, bpm);
+
+
+
+    }
+
+    // calculate standard deviation / Unstable rate
+    private Double calcStandardDeviation(HashSet<Long> inRange) {
+        double mean = 0;
+        double mean2 = 0;
+        for (Long clickTime : inRange) {
+            mean += clickTime;
+        }
+        mean = mean / inRange.size();
+        for (Long clickTime : inRange) {
+            mean2 += (clickTime - mean) * (clickTime - mean);
+        }
+        mean2 = mean2 / inRange.size();
+        mean2 = Math.sqrt(mean2);
+//        for (Long clickTime : inRange) mean += clickTime;
+//        mean /= inRange.size();
+//        for (Long clickTime : inRange) mean2 += Math.pow(clickTime - mean, 2);
+//        mean2 /= inRange.size();
+       // return Math.sqrt(mean2);
+        return mean2 / 10;
+    }
+
+    private void updateText(View v, double bpm) {
+        bpmText.setText("BPM: " + bpm);
         if (v.getId() == button1.getId()) {
-            bpmText.setText("BPM: " + bpm);
             button1.setText(String.valueOf(++b1clicks));
         } else {
-            bpmText.setText("BPM: " + bpm);
             button2.setText(String.valueOf(++b2clicks));
         }
         clicksText.setText("Clicks: " + clicks.size());
+        unstableRateText.setText("Unstable Rate: " + new DecimalFormat("0.#").format(UR));
     }
+
+
 
 
 }
